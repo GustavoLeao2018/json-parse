ffa7d10a3f3bb4cf43864b10eaf7ad873f5ec9ca
GustavoLeao2018 <gustavo.leao.nogueira.2017@gmail.com>
2019-03-07 15:26:06 -0300

    teste novo

--- js/exemplo.js
+++ js/exemplo.js
@@ -1,20 +1,7 @@
 $(document).ready(
   function(){
-    $.getJSON({
-      type: "GET",
-      headers: {"Access-Control-Allow-Origin" : "*"},
-      url: "js/db.json"
-    }, function(data){
-        $.each(data, function(key, value){
-            if(key == "nome"){
-              $("#nome").append("Nome: "+value);
-            }
-            if(key == "idade"){
-              $("#idade").append("Idade: "+value);
-            }
-            if(key == "facebook"){
-              $("#facebook").append('Facebook: <a href="'+value+'">Facebook</a>');
-            }
-        });
+    $.getJSON("", function(data){
+        console.log(data);
     });
+
   });
